344f6405c6010e5e0bfa4b314c8719f533b45142
Jakub Prymak <jprymak@gmail.com>
2020-06-11 18:42:20 +0200

    finished editor

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,19 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+const saveBtn = document.querySelector('.editor__save-button--js')
+const loadBtn = document.querySelector('.editor__load-button--js')
+let textArea = document.querySelector('.editor__textarea--js')
+
+
+saveBtn.addEventListener('click', ()=>{
+    
+    localStorage.setItem('1', textArea.value);
+})
+
+loadBtn.addEventListener('click', ()=>{
+    
+    textArea.value = localStorage.getItem('1');
+    
+})
--- webpack.common.js
+++ webpack.common.js
@@ -57,11 +57,6 @@ module.exports = {
       chunks: ["index"],
       filename: "index.html",
     }),
-    new HtmlWebpackPlugin({
-      template: "./src/pages/another.html",
-      inject: true,
-      chunks: ["index", "another"],
-      filename: "another.html",
-    }),
+    
   ],
 };
